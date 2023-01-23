1bdc6b21786827e5a333d2aa147c829cbae07746
MSiesto <miguelsiesto@gmail.com>
2023-01-23 10:00:06 +0100

    create state(counter) and Button(incrementcounter)

--- src/Components/Countes.tsx
+++ src/Components/Countes.tsx
@@ -1,9 +1,21 @@
-
+import {useState} from 'react'
 
 export const Counter = () => {
+
+   const [counter, setCounter] = useState(0)
+
+   const incrementar = () => {
+    setCounter(counter + 1)
+   }
+
 return (
     <div className="mt-5">
        <h3>Counter: useState</h3>
+       <span>Valor: {counter}</span>
+       <br/>
+       <button onClick={incrementar} className='btn btn-outline-primary mt-2' >
+        +1
+       </button>
     </div>
 )
 }
