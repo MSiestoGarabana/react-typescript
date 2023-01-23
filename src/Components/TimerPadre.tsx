67ceafc71b8d12ac4e3b7446cdd0f7bfe25fd8b1
MSiesto <miguelsiesto@gmail.com>
2023-01-23 12:23:58 +0100

    milisegundos state & new milisegundos button

--- src/Components/TimerPadre.tsx
+++ src/Components/TimerPadre.tsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Timer } from './Timer';
 
 export const TimerPadre = () => {
+
+    const [milisegundos, setMilisegundos] = useState(1000);
+
     return(
         <>
-        <span>Milisegundos { 1000 }</span>
+        <span>Milisegundos { milisegundos }</span>
 
        <br/>
 
-       <button className="btn btn-outline-success">1000</button>
+       <button className="btn btn-outline-success"
+               onClick={()=>setMilisegundos(1000) }>
+        1000
+        </button>
+
+       <button className="btn btn-outline-success"
+               onClick={()=>setMilisegundos(2000)}>
+        2000
+        </button>
 
         <Timer/>
         </>
