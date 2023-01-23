a4743c80816f01bea0fdf30098844bcf627a9292
MSiesto <miguelsiesto@gmail.com>
2023-01-23 12:15:41 +0100

    replacing setInterval with setTimeout

--- src/Components/Timer.tsx
+++ src/Components/Timer.tsx
@@ -1,7 +1,16 @@
+import {useEffect, useState} from "react";
+
 export const Timer = () => {
+    
+    const[segundos, setSegundos] = useState(0)
+
+    useEffect( ()=>{
+       setTimeout(() => setSegundos( segundos + 1) , 1000);
+    }, [segundos])
+
     return (
         <>
-        <h4>Timer:<small>0</small></h4>
+        <h4>Timer:<small>{ segundos }</small></h4>
         </>
     )
 }
--- src/Components/TimerPadre.tsx
+++ src/Components/TimerPadre.tsx
@@ -5,6 +5,11 @@ export const TimerPadre = () => {
     return(
         <>
         <span>Milisegundos { 1000 }</span>
+
+       <br/>
+
+       <button className="btn btn-outline-success">1000</button>
+
         <Timer/>
         </>
     )
