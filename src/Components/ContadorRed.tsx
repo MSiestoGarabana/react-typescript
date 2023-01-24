653adad140100226bee6e7860433e73b8ec757bc
MSiesto <miguelsiesto@gmail.com>
2023-01-24 10:30:42 +0100

    implementing "custom" actionType, involving payload

--- src/Components/ContadorRed.tsx
+++ src/Components/ContadorRed.tsx
@@ -1,3 +1,4 @@
+
 import { useReducer } from "react"
 const initialState = {
     contador:0,
@@ -6,6 +7,7 @@ const initialState = {
 type ActionType = 
      | { type: 'incrementar' }
      | { type: 'reducir'}
+     | { type: 'custom', payload: number};
 
 
 
@@ -23,6 +25,11 @@ const contadorReducer = ( state: typeof initialState, action: ActionType ) => {
                     ...state,
                     contador: state.contador - 1
                 }    
+            case 'custom':
+                return {
+                    ...state,
+                    contador: action.payload
+                }    
         default:
             return state;
     }
@@ -30,16 +37,20 @@ const contadorReducer = ( state: typeof initialState, action: ActionType ) => {
  
 export const ContadorRed = () => {
 
-    const [contadorState, dispatch] = useReducer(contadorReducer, initialState)
+    const [{ contador }, dispatch] = useReducer(contadorReducer, initialState)
   return (
     <>
-      <h2>Contador: {contadorState.contador}</h2>
+      <h2>Contador: { contador }</h2>
       <button className="btn btn-outline-primary" onClick={() => dispatch({ type : 'incrementar'})}>
         +1
       </button>
 
       <button className="btn btn-outline-primary" onClick={() => dispatch({ type : 'reducir'})}>
         -1
+      </button>
+
+      <button className="btn btn-outline-danger" onClick={() => dispatch({ type : 'custom', payload: 100})}>
+        100
       </button>
     </>
   )
