c2f39dcca24acb359837fad47a2744b59d6200dd
MSiesto <miguelsiesto@gmail.com>
2023-01-24 10:15:19 +0100

    creating useReducer & neccesary files to manage it

--- src/Components/ContadorRed.tsx
+++ src/Components/ContadorRed.tsx
@@ -1,10 +1,46 @@
+import { useReducer } from "react"
+const initialState = {
+    contador:0,
+}
+
+type ActionType = 
+     | { type: 'incrementar' }
+     | { type: 'reducir'}
 
 
+
+const contadorReducer = ( state: typeof initialState, action: ActionType ) => {
+    
+    switch (action.type) {
+        case 'incrementar':
+            return {
+                ...state,
+                contador: state.contador + 1
+            }
+
+            case 'reducir':
+                return {
+                    ...state,
+                    contador: state.contador - 1
+                }    
+        default:
+            return state;
+    }
+}
  
 export const ContadorRed = () => {
+
+    const [contadorState, dispatch] = useReducer(contadorReducer, initialState)
   return (
     <>
-      <h2>Contador: 10</h2>
+      <h2>Contador: {contadorState.contador}</h2>
+      <button className="btn btn-outline-primary" onClick={() => dispatch({ type : 'incrementar'})}>
+        +1
+      </button>
+
+      <button className="btn btn-outline-primary" onClick={() => dispatch({ type : 'reducir'})}>
+        -1
+      </button>
     </>
   )
 }
