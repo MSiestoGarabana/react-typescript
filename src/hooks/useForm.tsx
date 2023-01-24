881d27bb98155044d4ee74e410d1d6bf78ad86c7
MSiesto <miguelsiesto@gmail.com>
2023-01-24 11:26:33 +0100

    creating useForms

---
+++ src/hooks/useForm.tsx
@@ -0,0 +1,21 @@
+import { useState, ChangeEvent } from "react";
+
+
+
+export const useForm = ( initState: any) => {
+    
+    const [formulario, setFormulario]= useState(initState);
+
+    const handleChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+       const { name, value } = target;
+       
+       setFormulario({
+        ...formulario,
+        [ name ]: value
+       })
+    }
+    return {
+        formulario,
+        handleChange
+    }
+}
